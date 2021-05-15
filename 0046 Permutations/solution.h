5aa7f07886a0f004b56e244def897ed2ac7eebed
sheldonjinqi <37092479+sheldonjinqi@users.noreply.github.com>
2021-05-15 16:40:51 -0400

    Update solution.h

--- 0046 Permutations/solution.h
+++ 0046 Permutations/solution.h
@@ -4,14 +4,14 @@ public:
     void permute_helper(vector<int>& nums, int first, int right_p){
         //basecase: no swap, add the array to the 2d vector
         if(first==right_p){
-            rslt_vec.push_back(nums);
+            rslt_vec.push_back(nums); 
         }
         else{
+            // loop through each possible swap
             for(int i=first; i<right_p+1; i++){
-                // cout<<"i: "<<i<<"\t first"<<first<<endl;
                 swap(nums[first],nums[i]);
-                permute_helper(nums, first+1, right_p);
-                swap(nums[first],nums[i]);
+                permute_helper(nums, first+1, right_p); //move the first to next place 
+                swap(nums[first],nums[i]); //swap back to keep the original order for next operation
             }
         }
     }
