19916d1ce6ff243ef0e4dba5a82edbd59c521c71
sheldonjinqi <37092479+sheldonjinqi@users.noreply.github.com>
2021-05-19 14:15:13 -0400

    Update solution.h
    Added comments

--- 0045 Jump Game II/solution.h
+++ 0045 Jump Game II/solution.h
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int count =0, curr_end=0, farthest_idx=0;
-        // greedy method, always take the jump leads to the furthest
-        if(nums.size()==1){
-            return 0;
-        }
+        
+        //greedy method, always jump to the furthest if possible. 
+        if(nums.size()==1) return 0;
+        int end = 0; //point to the fathest idx can be reached 
+        int end_curr =0 ; //point to the farthest idx can be reached in this step
+        int jump =0;
         for(int i=0;i<nums.size()-1;i++){
-            farthest_idx = max(farthest_idx,i+nums[i]);
-            if(i == curr_end){
-                curr_end = farthest_idx;
-                count++;
+            // find the furthest idx can be reached from currently available idx.
+            end = max(end,i+nums[i]);
+            if(i==end_curr) {
+                end_curr = end; //update the furthest idx can be reached from previous points
+                jump++;
             }
         }
-        return count;
-
+        return jump;
+        
     }
 };
