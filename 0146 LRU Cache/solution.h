f53ffeb0f61aac872e4bde8e39d8a5bb2f720b6d
sheldonjinqi <37092479+sheldonjinqi@users.noreply.github.com>
2021-05-19 14:32:03 -0400

    Update solution.h

--- 0146 LRU Cache/solution.h
+++ 0146 LRU Cache/solution.h
@@ -12,23 +12,25 @@ public:
     int get(int key) {
         if(CacheMap.count(key)){
             cacheList.splice(cacheList.begin(),cacheList,CacheMap[key]);
-            // CacheMap[key] = cacheList.begin(); 
             return CacheMap[key]->second;
         }
         return -1;
     }
     
     void put(int key, int value) {
+        // if the pair already exists, move it to the front and update the value
         if(CacheMap.count(key)){
             cacheList.splice(cacheList.begin(),cacheList,CacheMap[key]);
             CacheMap[key]->second = value; //update the value of given key
             // CacheMap[key] = cacheList.begin(); 
         }
+        // if list is not full, add key:val pair to the front
         else if (size<map_capacity){
             cacheList.push_front(pair<int,int> {key,value});
             CacheMap[key] = cacheList.begin();
             size++;
         }
+        //if list is full, add new pair to the front, remove the pair from the end of list.
         else{
             CacheMap.erase(cacheList.back().first); 
             cacheList.pop_back();
